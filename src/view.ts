f0d66827ac107bf734ce3227fcac9126f5f3d971
Partisan1 <128032116+Partisan1@users.noreply.github.com>
2025-03-12 09:41:45 +0100

    Update view.ts

--- src/view.ts
+++ src/view.ts
@@ -1,3 +1,6 @@
+import { startWord, endWord} from "./model.js";
+
+
 export function renderGame(): void {
     const form = document.querySelector("form") as HTMLElement;
     form.innerHTML = "";
@@ -30,11 +33,46 @@ export function renderGame(): void {
 }
 
 
+
 export function clearInput(): void {
     const input = document.querySelector("#intermediate-word") as HTMLInputElement;
     input.value = "";
     input.focus();
 }
+
+export function getInputValue(){
+    const input = document.querySelector("#intermediate-word") as HTMLInputElement;
+    const inputValue = input.value;
+    return inputValue;
+}
+
+export function validation(startWord: string, endWord: string, inputValues: string[], inputField: HTMLInputElement): void {
+    const input = inputField.value.trim();
+
+    if (input.length !== 3 || input.includes(" ")) {
+        alert("Érvénytelen szó! Csak 3 betűs szavakat adj meg!");
+        inputField.value = "";
+        inputField.focus();
+        return;
+    }
+
+    if (inputValues.includes(input)) {
+        alert("Ez a szó már szerepel!");
+        inputField.value = "";
+        inputField.focus();
+        return;
+    }
+
+    inputValues.push(input);
+
+    startWord = input;
+
+    if (input === endWord) {
+        inputField.disabled = true;
+        alert("Gratulálok, elérted a célszót!");
+    }
+}
+
 
 export function disableInput(): void {
     (document.querySelector("#intermediate-word") as HTMLInputElement).disabled = true;
@@ -44,23 +82,8 @@ export function enableStartButton(): void {
     (document.querySelector(".start") as HTMLButtonElement).disabled = false;
 }
 
-export function validation(startWord:string, endWord:string, inputValues:string[]) {
-    const inputField = document.getElementById("intermediate-word") as HTMLInputElement;
-    const input = inputField.value;
-    inputField.addEventListener("change",()=>{
-        if (input.length < 3 || input.includes(" ")) {
-            alert("Érvénytelen szó! Csak 3 betűs szavakat adj meg!");
-            inputField.value = "";
-            inputField.focus();
-            return;
-        }
-        
-        inputValues.push(input);
-        startWord = input;
-        
-        if (input === endWord) {
-            inputField.disabled = true;
-            alert("Gratulálok, elérted a célszót!");
-        }
-    })
-}
+
+
+
+console.log(startWord);
+console.log(endWord);
