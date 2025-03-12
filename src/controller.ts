da4dd72d8111f25f2a18b6c829ba830254cc6114
Agoston141 <128032262+Agoston141@users.noreply.github.com>
2025-03-12 09:41:19 +0100

    Update controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -1,16 +1,15 @@
-import { endWord, generateWords, inputValues, startWord} from "./model.js"
-import { disableInput, renderGame,validation } from "./view.js"
+import { endWord, generateWords, inputValues, startWord,validation} from "./model.js"
+import { disableInput, enableStartButton, renderGame, } from "./view.js"
 
 window.addEventListener("DOMContentLoaded",init)
 
+function init(){ 
+    const button= document.querySelector(".start")?.addEventListener("click",buttonclickhandler)
+}
 
-
-function init(){
-    
-    const button= document.querySelector(".start")?.addEventListener("click",()=>{
-        console.log(startWord);
-        generateWords()
-        renderGame(startWord,endWord)
-        validation(startWord,endWord,inputValues)
-    })
+function buttonclickhandler(){
+    generateWords()
+    renderGame(startWord,endWord)
+    validation(startWord,endWord,inputValues)
+    enableStartButton()
 }
