942c546d36192bdc1a51e13da0a01b668a3e693a
Agoston141 <128032262+Agoston141@users.noreply.github.com>
2025-03-12 09:41:40 +0100

    Update model.ts

--- src/model.ts
+++ src/model.ts
@@ -1,50 +1,47 @@
-const words = ["ház", "kár", "lap", "nap", "rak", "tál", "fut", "rög", "döf", "nép", "kép", "tol", "húz", "bál", "jár", "rák", "láb", "tép", "kút", "búg", "túr", "lép", "súg", "nől", "fől","baj", "vaj", "haj", "raj","víg", "díj", "vív", "fal", "hal", "hév", "vad", "vád", "rag", "fűt","fűz", "fúr", "dúl", "dől", "kár", "tőr", "lúd", "lég", "lát", "sál"];
+export function renderGame(startWord:string,endWord:string): void {
+    const form = document.querySelector("form") as HTMLElement;
+    form.innerHTML = "";
 
-export let startWord = "";
-export let endWord = "";
-export let inputValues: string[] = [];
+    const inputContainer = document.createElement("div");
+    inputContainer.classList.add("input-elements");
 
-function getRandomIndex(max: number): number {
-    return Math.floor(Math.random() * max);
+    const startWordInput = document.createElement("input");
+    startWordInput.type = "text";
+    startWordInput.id = "start-word";
+    startWordInput.disabled = true;
+    startWordInput.value = startWord;
+
+    const intermediateWordInput = document.createElement("input");
+    intermediateWordInput.type = "text";
+    intermediateWordInput.id = "intermediate-word";
+    intermediateWordInput.maxLength = 3;
+
+    const endWordInput = document.createElement("input");
+    endWordInput.type = "text";
+    endWordInput.id = "end-word";
+    endWordInput.disabled = true;
+    endWordInput.value = endWord;
+
+    inputContainer.appendChild(startWordInput);
+    inputContainer.appendChild(intermediateWordInput);
+    inputContainer.appendChild(endWordInput);
+
+    form.appendChild(inputContainer);
 }
 
-export function generateWords(){
-    const startIndex = getRandomIndex(words.length);
-    const endIndex = getRandomIndex(words.length);
 
-    startWord = words[startIndex];
-    endWord = words[endIndex];
-    
+export function clearInput(): void {
+    const input = document.querySelector("#intermediate-word") as HTMLInputElement;
+    input.value = "";
+    input.focus();
 }
 
-export function validation(startWord:string, endWord:string, inputValues:string[]) {
-    const inputField = document.getElementById("intermediate-word") as HTMLInputElement;
-    
-    inputField.addEventListener("change",()=>{
-        const input = inputField.value;
-        if (input.length < 3 || input.includes(" ")) {
-            alert("Érvénytelen szó! Csak 3 betűs szavakat adj meg!");
-            inputField.value = "";
-            inputField.focus();
-            
-            return;
-        }
-        else if (input === endWord) {
-            inputField.disabled = true;
-            alert("Gratulálok, elérted a célszót!");
-        }
-        else{
-            inputField.value = "";
-            inputField.focus();
-            inputValues.push(input);
-            startWord = input;
-        }
-        
-        
-        
-    })
+export function disableInput(): void {
+    (document.querySelector("#intermediate-word") as HTMLInputElement).disabled = true;
 }
 
-export function ValidateIfcorrect(){
-    const inputField = document.getElementById("intermediate-word") as HTMLInputElement;
+export function enableStartButton(): void {
+    const startBTN = document.querySelector(".start") as HTMLButtonElement;
+    startBTN.disabled = false;
 }
+
