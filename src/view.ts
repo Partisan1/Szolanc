bff8fb8013ef7484547738da956b3823a9d69eb4
Partisan1 <128032116+Partisan1@users.noreply.github.com>
2025-03-12 08:51:03 +0100

    Update view.ts

--- src/view.ts
+++ src/view.ts
@@ -2,16 +2,36 @@ import { startWord, endWord, validation } from "./model.js";
 
 
 export function renderGame(): void {
-        const form = document.querySelector("form") as HTMLElement;
-        form.innerHTML = `
-            <div class="input-elements">
-                <input type="text" id="start-word" disabled value="${startWord}">
-                <input type="text" id="intermediate-word" maxlength="3">
-                <input type="text" id="end-word" disabled value="${endWord}">
-            </div>
-            
-        `
-    }
+    const form = document.querySelector("form") as HTMLElement;
+    form.innerHTML = "";
+
+    const inputContainer = document.createElement("div");
+    inputContainer.classList.add("input-elements");
+
+    const startWordInput = document.createElement("input");
+    startWordInput.type = "text";
+    startWordInput.id = "start-word";
+    startWordInput.disabled = true;
+    startWordInput.value = startWord;
+
+    const intermediateWordInput = document.createElement("input");
+    intermediateWordInput.type = "text";
+    intermediateWordInput.id = "intermediate-word";
+    intermediateWordInput.maxLength = 3;
+
+    const endWordInput = document.createElement("input");
+    endWordInput.type = "text";
+    endWordInput.id = "end-word";
+    endWordInput.disabled = true;
+    endWordInput.value = endWord;
+
+    inputContainer.appendChild(startWordInput);
+    inputContainer.appendChild(intermediateWordInput);
+    inputContainer.appendChild(endWordInput);
+
+    form.appendChild(inputContainer);
+}
+
 
 
 export function clearInput(): void {
