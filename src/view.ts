4cc6203705ab19747dcba292fe420310a5408ca9
Agoston141 <128032262+Agoston141@users.noreply.github.com>
2025-03-12 09:14:19 +0100

    Update view.ts

--- src/view.ts
+++ src/view.ts
@@ -1,37 +1,14 @@
-import { startWord, endWord, validation } from "./model.js";
-
-
-export function renderGame(): void {
-    const form = document.querySelector("form") as HTMLElement;
-    form.innerHTML = "";
-
-    const inputContainer = document.createElement("div");
-    inputContainer.classList.add("input-elements");
-
-    const startWordInput = document.createElement("input");
-    startWordInput.type = "text";
-    startWordInput.id = "start-word";
-    startWordInput.disabled = true;
-    startWordInput.value = startWord;
-
-    const intermediateWordInput = document.createElement("input");
-    intermediateWordInput.type = "text";
-    intermediateWordInput.id = "intermediate-word";
-    intermediateWordInput.maxLength = 3;
-
-    const endWordInput = document.createElement("input");
-    endWordInput.type = "text";
-    endWordInput.id = "end-word";
-    endWordInput.disabled = true;
-    endWordInput.value = endWord;
-
-    inputContainer.appendChild(startWordInput);
-    inputContainer.appendChild(intermediateWordInput);
-    inputContainer.appendChild(endWordInput);
-
-    form.appendChild(inputContainer);
-}
-
+export function renderGame(startWord:string,endWord:string): void {
+        const form = document.querySelector("form") as HTMLElement;
+        form.innerHTML += `
+            <div class="input-elements">
+                <input type="text" id="start-word" disabled value="${startWord}">
+                <input type="text" id="intermediate-word" maxlength="3">
+                <input type="text" id="end-word" disabled value="${endWord}">
+            </div>
+            
+        `
+    }
 
 
 export function clearInput(): void {
@@ -48,8 +25,23 @@ export function enableStartButton(): void {
     (document.querySelector(".start") as HTMLButtonElement).disabled = false;
 }
 
-
-
-
-console.log(startWord);
-console.log(endWord);
+export function validation(startWord:string, endWord:string, inputValues:string[]) {
+    const inputField = document.getElementById("intermediate-word") as HTMLInputElement;
+    const input = inputField.value;
+    inputField.addEventListener("change",()=>{
+        if (input.length < 3 || input.includes(" ")) {
+            alert("Érvénytelen szó! Csak 3 betűs szavakat adj meg!");
+            inputField.value = "";
+            inputField.focus();
+            return;
+        }
+        
+        inputValues.push(input);
+        startWord = input;
+        
+        if (input === endWord) {
+            inputField.disabled = true;
+            alert("Gratulálok, elérted a célszót!");
+        }
+    })
+}
